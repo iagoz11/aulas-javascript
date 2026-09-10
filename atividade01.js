fba2e6282d3c337daff80c49d7544df2d2b9a2a1
IAGO DA SILVA LANDMANN <iago_landmann@estudante.sesisenai.org.br>
2026-09-10 20:03:41 -0300

    Atividade de cadastro de itens

--- atividade01.js
+++ atividade01.js
@@ -1,4 +1,4 @@
-//Etapa 1
+//Etapa 1 Ficha do Item
 
 let nomeItem = "Espada das Sombras";
 let precoItem = 250;
@@ -11,7 +11,7 @@ Preço: R$ ${precoItem}
 Raridade: ${raridadeItem}
 Quantidade no estoque: ${quantidadeEstoque}`);
 
-//Etapa 2
+//Etapa 2 Regras de Negócio
 
 if(precoItem >= 0) {
     console.log("Preço válido!");
@@ -33,7 +33,7 @@ console.log(`Valorzinho muito alto né: ${emDestaque}`);
 let disponivelCompra = quantidadeEstoque > 0 && precoItem > 0;
 console.log(`Disponível para compra: ${disponivelCompra}`);
 
-//Etapa 3
+//Etapa 3 Cadastro em Lote
 
 for (let i = 1; i <= 5; i++){
     console.log(`Item ${i} cadastrado.`);
@@ -43,7 +43,7 @@ for (let i = 1; i <= 5; i++){
     }
 }
 
-//Etapa 4
+//Etapa 4 Controle de Estoque
 
 let estoque = 12;
 let vendas = 0;
@@ -55,7 +55,7 @@ while (estoque > 0) {
     console.log(`Uma venda foi realizada. Estoque restante: ${estoque}`);
 }
 
-//Etapa 5
+//Etapa 5 Catálogo de Itens
 
 const catalogo = ["Espada das sombras", "Asas douradas", "Capacete neon", "Pet dragão", "Skin cyberpunk"];
 
